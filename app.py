9677f7177c56477ed20691fafece1d37a56498bf
Marcosavi <marcoscsavi@gmail.com>
2025-05-29 18:16:55 +0300

    update: structure

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from flask import Flask, render_template
+from app import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-"""
-avoid the need to always call:
-flask run to reflect the changes
-"""
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(debug=True)
