e447b9b5e925f8458d0e18262eb5012892660891
Marcosavi <marcoscsavi@gmail.com>
2025-06-11 22:49:20 +0300

    update: change in js and style

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -12,76 +12,87 @@ const mapContainer = document.getElementById('map-container');
 const mapImage = document.getElementById('map');
 const timeDisplay = document.getElementById('current-time');
 
+/*important not for exxporting csv, but 
+to draw the line */
 let points = [];
 
+/*make sure that exportation is not allowed if no points*/
+document.getElementById('exportBtn').disabled = true;
+
 function startMapping() {
+    const startBtn = document.getElementById('startBtn');
+
+    if (startBtn.classList.contains('reset-mode')) {
+        location.reload(); return;}
+
     startTime = Date.now() - elapsed;
     timerRunning = true;
-    document.getElementById('startBtn').disabled = true;
+    startBtn.disabled = true;
     document.getElementById('pauseBtn').disabled = false;
-
-    intervalId = setInterval(updateTime, 100); // update every 100ms
+    intervalId = setInterval(updateTime, 100);
 }
 
+/* handling the fact to allow users to pause*/
 function togglePause() {
-    if (timerRunning) {
-        elapsed = Date.now() - startTime;
+    if (timerRunning) {elapsed = Date.now() - startTime;
         clearInterval(intervalId);
     } else {
         startTime = Date.now() - elapsed;
         intervalId = setInterval(updateTime, 100);
-    }
-    timerRunning = !timerRunning;
-}
+    } timerRunning = !timerRunning}
 
 function updateTime() {
     const currentTime = ((Date.now() - startTime) / 1000).toFixed(2);
-    timeDisplay.textContent = currentTime;
-}
+    timeDisplay.textContent = currentTime;}
 
+/* activated when the user clicks in the button export
+then, generate the csv and redirect the user */
 function exportCSV() {
     window.location.href = '/export';
 }
 
-/* very important for */
+/* very important for passing sending the location to
+the API that is inside routes.py */
 async function sendLocation(time, x, y) {
     await fetch('/add-location', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ time, x, y })
-    });
-}
+        body: JSON.stringify({ time, x, y })})}
 
 mapContainer.addEventListener('dblclick', function(event) {
+    /* simply don't draw if timer is not running */
     if (!timerRunning) return;
-
     const rect = mapImage.getBoundingClientRect();
     const x = (event.clientX - rect.left) / rect.width;
     const y = (event.clientY - rect.top) / rect.height;
     const timeSeconds = ((Date.now() - startTime) / 1000).toFixed(2);
-
     const naturalWidth = 1946;
     const naturalHeight = 1574;
     const pixelX = Math.round(x * naturalWidth);
     const pixelY = Math.round(y * naturalHeight);
-
     const icon = document.createElement('div');
     icon.classList.add('location-icon');
     icon.style.left = `${x * 100}%`;
-    icon.style.top = `${y * 100}%`;
+    icon.style.top = `${y * 105}%`;
     mapContainer.appendChild(icon);
-
     sendLocation(timeSeconds, pixelX.toFixed(4), pixelY.toFixed(4));
-
     const relativeX = event.clientX - rect.left;
     const relativeY = event.clientY - rect.top;
     points.push({ x: relativeX, y: relativeY });
-
+    if (points.length > 0) {
+        document.getElementById('exportBtn').disabled = false;
+    }
     if (points.length >= 2) {
         const prev = points[points.length - 2];
         const curr = points[points.length - 1];
         drawLine(prev.x, prev.y, curr.x, curr.y);
     }
+
+    const startBtn = document.getElementById('startBtn');
+    startBtn.textContent = "Reset Mapping";
+    startBtn.classList.add('reset-mode');
+    startBtn.disabled = false;
+    
 });
 
 /* function used to draw the line
