3ead2daa6c23b1db572f614f6ea4009a6b49591a
Marcosavi <marcoscsavi@gmail.com>
2025-06-13 18:02:15 +0300

    fix: remove the need for dbs

--- application.py
+++ application.py
@@ -1,29 +1,28 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-import secrets
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_login import LoginManager
+#import secrets
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+#db = SQLAlchemy()
+#login_manager = LoginManager()
 
 def create_app():
     #standard for all flask applications
     app = Flask(__name__)
     
     #config my database, as seen in codecademy flask course
-    app.config["SECRET_KEY"] = secrets.token_hex(16)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    #app.config["SECRET_KEY"] = secrets.token_hex(16)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
     #basically begins the app
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = "main.login"
+    #db.init_app(app)
+    #login_manager.init_app(app)
+    #login_manager.login_view = "main.login"
+    #from app.models import User
 
-    from app.models import User
-
-    @login_manager.user_loader
+    """@login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        return User.query.get(int(user_id))"""
 
     #replace the app
     from app.main import bp
